4771289ca6401a78c1561360979746f823c71fc8
Sarah-Albalawi <s.albalawi@outlook.com>
2022-10-27 14:51:03 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
+ 
   // your code here
-});
+     let form = document.querySelector('form');
+     form.addEventListener('submit', (e) =>{
+      e.preventDefault();
+      //console.log(e.target);
+
+      handleToDo(e.target.input.value);
+      form.reset();
+    });
+     })
+   
+
+    function handleToDo(list){
+      let para = document.createElement('li');
+      let btn = document.createElement('button');
+      btn.textContent ='DELETE';
+      para.textContent = `${list} `;
+      para.appendChild(btn);
+      console.log(para);
+    let toDos = document.getElementById('tasks');
+    toDos.appendChild(para)
+      btn.addEventListener('click', handleDelete)
+    } 
+ function handleDelete(e){
+         e.target.parentNode.remove();
+ }
